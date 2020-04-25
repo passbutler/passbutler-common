62cbaa432cf2c141499d8b97e90121dee954aaf7
Bastian Raschke <bastian.raschke@posteo.de>
2020-04-25 15:37:46 +0200

    Merge review adjustments

--- src/main/kotlin/de/passbutler/common/base/UriExtensions.kt
+++ src/main/kotlin/de/passbutler/common/base/UriExtensions.kt
@@ -1,6 +1,16 @@
 package de.passbutler.common.base
 
+import org.tinylog.kotlin.Logger
 import java.net.URI
 
 val URI.isHttpsScheme
     get() = scheme == "https"
+
+fun String.toURIOrNull(): URI? {
+    return try {
+        URI.create(this)
+    } catch (exception: Exception) {
+        Logger.warn(exception, "The URI '$this' could not be parsed!")
+        null
+    }
+}
