d2e35969d8250ae33019f28c2f4626b40b1732bd
Bastian Raschke <bastian.raschke@posteo.de>
2020-05-25 13:42:15 +0200

    Added another list extension

--- src/main/kotlin/de/passbutler/common/base/ListExtensions.kt
+++ src/main/kotlin/de/passbutler/common/base/ListExtensions.kt
@@ -5,3 +5,11 @@ fun <T> MutableList<T>.addAllIfNotNull(elements: Collection<T>?) {
         addAll(elements)
     }
 }
+
+fun <T> Collection<T>.contains(block: (T) -> Boolean): Boolean {
+    return find(block) != null
+}
+
+fun <T> Collection<T>.containsNot(block: (T) -> Boolean): Boolean {
+    return !contains(block)
+}
