db34c4add380df348e0022336475685ed8a40aa4
Bastian Raschke <bastian.raschke@posteo.de>
2020-11-14 12:25:59 +0100

    Moved date time formatting extension

---
+++ src/main/kotlin/de/passbutler/common/base/DateExtensions.kt
@@ -0,0 +1,16 @@
+package de.passbutler.common.base
+
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.*
+
+val Instant.formattedDateTime: String
+    get() {
+        val dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
+            .withLocale(Locale.getDefault())
+            .withZone(ZoneId.systemDefault())
+
+        return dateTimeFormatter.format(this)
+    }
