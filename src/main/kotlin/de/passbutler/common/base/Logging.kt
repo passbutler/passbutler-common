f387c0e3e1e544598bd8139d653756627d0692de
Bastian Raschke <bastian.raschke@posteo.de>
2020-11-21 19:15:32 +0100

    Added log constant

---
+++ src/main/kotlin/de/passbutler/common/base/Logging.kt
@@ -0,0 +1,5 @@
+package de.passbutler.common.base
+
+object LoggingConstants {
+    const val LOG_FORMAT_FILE = "{date:yyyy-MM-dd HH:mm:ss.SSS} {level} {class-name}.{method}() [{thread}]: {message}"
+}
