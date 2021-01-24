324e98646635677aa601c1d35c02b663ca77dced
Bastian Raschke <bastian.raschke@posteo.de>
2021-01-24 16:12:48 +0100

    Removed unnecessary types

--- src/test/kotlin/de/passbutler/common/base/ByteArrayExtensionsTest.kt
+++ src/test/kotlin/de/passbutler/common/base/ByteArrayExtensionsTest.kt
@@ -18,14 +18,14 @@ class ByteArrayExtensionsTest {
     @Test
     fun `Compares a null ByteArray and non-null ByteArray results that they are not equal`() {
         val firstByteArray: ByteArray? = null
-        val secondByteArray: ByteArray? = ByteArray(1)
+        val secondByteArray = ByteArray(1)
 
         assertArrayNotEquals(firstByteArray, secondByteArray)
     }
 
     @Test
     fun `Compares a non-null ByteArray and null ByteArray results that they are not equal`() {
-        val firstByteArray: ByteArray? = ByteArray(1)
+        val firstByteArray = ByteArray(1)
         val secondByteArray: ByteArray? = null
 
         assertArrayNotEquals(firstByteArray, secondByteArray)
@@ -33,24 +33,24 @@ class ByteArrayExtensionsTest {
 
     @Test
     fun `Compares two equal non-null ByteArrays results that they are equal`() {
-        val firstByteArray: ByteArray? = "AABBCC".hexToBytes()
-        val secondByteArray: ByteArray? = "AABBCC".hexToBytes()
+        val firstByteArray = "AABBCC".hexToBytes()
+        val secondByteArray = "AABBCC".hexToBytes()
 
         assertArrayEquals(firstByteArray, secondByteArray)
     }
 
     @Test
     fun `Compares two different in size ByteArrays results that they are not equal`() {
-        val firstByteArray: ByteArray? = "AABBCC".hexToBytes()
-        val secondByteArray: ByteArray? = "AABBCCDD".hexToBytes()
+        val firstByteArray = "AABBCC".hexToBytes()
+        val secondByteArray = "AABBCCDD".hexToBytes()
 
         assertArrayNotEquals(firstByteArray, secondByteArray)
     }
 
     @Test
     fun `Compares two different in content ByteArrays results that they are not equal`() {
-        val firstByteArray: ByteArray? = "AABBCC".hexToBytes()
-        val secondByteArray: ByteArray? = "AABBDD".hexToBytes()
+        val firstByteArray = "AABBCC".hexToBytes()
+        val secondByteArray = "AABBDD".hexToBytes()
 
         assertArrayNotEquals(firstByteArray, secondByteArray)
     }
